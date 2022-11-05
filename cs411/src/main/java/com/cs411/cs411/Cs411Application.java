9d22b7a70c635a7620cfc07667c33050d8dabed8
alyssacgoins <80764587+alyssacgoins@users.noreply.github.com>
2022-11-05 14:12:58 -0400

    Spring Boot Project

---
+++ cs411/src/main/java/com/cs411/cs411/Cs411Application.java
@@ -0,0 +1,13 @@
+package com.cs411.cs411;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs411Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs411Application.class, args);
+	}
+
+}
---
+++ cs411/src/test/java/com/cs411/cs411/Cs411ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs411.cs411;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs411ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
