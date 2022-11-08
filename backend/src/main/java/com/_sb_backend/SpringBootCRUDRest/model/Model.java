b10213d6a2ab2b610d892f693b1fff16c60a82fa
alyssacgoins <alyssacg@bu.edu>
2022-11-08 11:44:47 -0500

    Tuesday Overview

--- backend/src/main/java/com/_sb_backend/SpringBootCRUDRest/model/Model.java
+++ backend/src/main/java/com/_sb_backend/SpringBootCRUDRest/model/Model.java
@@ -1,5 +1,15 @@
 package com._sb_backend.SpringBootCRUDRest.model;
 
 public class Model {
-    
+    // endpoint --> soccer API
+        // 
+
+    // searching within exisiting database:
+        // (1) get
+
+    // user information:
+        // (1) put
+        // (2) post: ie. updating user informa
+        // (3) delete
+        // ie. brewery info, past games --> (going into a Mongo DB)
 }
