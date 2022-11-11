adc11066d79af6dadd3a7ff1b3476a47db5c6757
Jordan Tab <taboglut@bu.edu>
2022-11-10 23:54:14 -0500

    Added functionality to the search bar. Users can get a list of breweires based on home and away teams

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,86 @@
-import React from 'react';
+import {useRef} from 'react';
+import axios from 'axios';
+
+
+const apiKey = '99STUUcDXZs8D6AMKU0RUsqENtx6z5n6UJ1oqm8tB3Mfv2gLLmIH20LHig8X'
+const appKey = '5ecde912845e9b21bba7160f8dbd0388'
 
 function SearchBar() {
+    const homeRef = useRef();
+    const awayRef = useRef();
+
+    const onSubmit = () => {
+        
+        const req1 = axios.get(`https://cors-anywhere.herokuapp.com/https://api.sportmonks.com/v3/football/teams/search/${homeRef.current.value}?api_token=${apiKey}`).then((response => {
+            const homeID = response.data.data[0].id
+            getAwayID(homeID)
+        }
+        ))
+    }
+    
+    const getAwayID = (homeID) => {
+            const req2 = axios.get(`https://cors-anywhere.herokuapp.com/https://api.sportmonks.com/v3/football/teams/search/${awayRef.current.value}?api_token=${apiKey}`).then((response => {
+            const awayID = response.data.data[0].id
+            getVenueID(homeID,awayID)
+        }
+        ))
+    }
+
+    const getVenueID = (homeID,awayID) => {
+        const req3 = axios.get(`https://cors-anywhere.herokuapp.com/https://api.sportmonks.com/v3/football/fixtures/head-to-head/${homeID}/${awayID}?api_token=${apiKey}`).then((response => {
+            const venueID = response.data.data[0].venue_id
+            getCoordinates(venueID)
+        }))
+    }
+
+    const getCoordinates = (venueID) => {
+        const req4 = axios.get(`https://cors-anywhere.herokuapp.com/https://api.sportmonks.com/v3/football/venues/${venueID}?api_token=${apiKey}`).then((response => {
+            const latitude = response.data.data.latitude
+            const longitude = response.data.data.longitude
+            getBreweries(latitude,longitude)
+        }))
+    }
+
+    const getBreweries = (latitude,longitude) => {
+        const req5 = axios.get(`https://cors-anywhere.herokuapp.com/https://api.openbrewerydb.org/breweries?by_dist=${latitude},${longitude}`).then((response => {
+            const breweries = response.data
+            console.log(breweries)
+        }))
+    }
+
+    // const getFixture = (teamID) => { 
+    //     const gameDay = new Date(dateRef.current.value)
+    //     const nextDay = new Date(gameDay)
+    //     nextDay.setDate(gameDay.getDate() + 1);
+    //     const start_date = gameDay.toISOString().substring(0,10)
+    //     const end_date = nextDay.toISOString().substring(0,10)
+    //     console.log(typeof gameDay)
+    //     const req2 = axios.get(`https://cors-anywhere.herokuapp.com/https://api.sportmonks.com/v3/football/fixtures/between/${start_date}/${start_date}/${teamID}?api_token=${apiKey}`).then((response => {
+    //         const venueID = response.data
+    //         console.log(venueID)
+    //     }))
+
+    // } 
+
     return (
-        <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Enter Match </span>
-        </label>
+        <div> 
+            {/* Add instructions for complete team name */}       
         <input
             type="text"
             id="header-search"
-            placeholder="Team Name"
-            name="s" 
+            placeholder="Home"
+            name="home"
+            ref={homeRef} 
         />
         <input
             type="text"
             id="header-search"
-            placeholder="Date"
-            name="s" 
+            placeholder="Away"
+            name="away" 
+            ref={awayRef}
         />
-        <button type="submit">Search</button>
-    </form>
+        <button onClick={ () => onSubmit()}>Search</button>
+    </div>
     );
 }
 
