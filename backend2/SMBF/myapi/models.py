d57ef2d24b4695fa7d1401da26c78a464ad767e5
alyssacgoins <alyssacg@bu.edu>
2022-12-01 11:06:37 -0500

    Wednesday Work

--- backend2/SMBF/myapi/models.py
+++ backend2/SMBF/myapi/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
+import uuid
+from typing import Optional
+from pydantic import Basemodel, Field
+
 # Create your models here.
 class User(models.Model):
+    id:  str = Field(default_factory=uuid.uuid4, alias="_id")
+    username: str = Field(...)
+    password: str = Field(...)
+
+    # not sure what to do w the stuff directly below
     name = models.CharField(max_length=60)
 
     def __str__(self):
         return self.name
 
-# ------------------- added 11/27
-class Dweet(models.Model):
-    user = models.ForeignKey(
-        User, related_name="user", on_delete=models.DO_NOTHING
-    )
-    body = models.CharField(max_length=140)
-    created_at = models.DateTimeField(auto_now_add=True)
-    # --- store: game & date & time created at
-    games = models
+class UserUpdate(UserModel): 
+    title: Optional[str]
+    username: Optional[str]
+    password: Optional[str]
